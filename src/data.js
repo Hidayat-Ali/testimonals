030a31b5b0b466ba606cdc5e093e1d196f678fda
HidayatAliMir <meerhidayat563@gmail.com>
2021-07-17 12:17:48 +0530

    initial comit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'Hidayat Ali',
+      job: 'web developer',
+      image:
+        './Hidayat2.jpeg',
+      text:
+' hi this Hidayat from kashmir a web developer at the  SRM Easwari Engineering College and i am very interseted in Learning New things '    },
+    {
+      id: 2,
+      name: 'Mahmood Hussain',
+      job: 'Software Developer',
+      image:
+        './mehmood.jpeg',
+      text:
+        'Hi this this Mahmood Hussain a software Developer in China at Tapi University, and i always welcome to learn new things',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      text:
+        'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      text:
+        'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+  
+  export default reviews;
+  
